0fd7fcbf972e75db452b831941ff7396d8580523
Bardia Zamani <barzamsr@bardias-air.home>
2021-04-17 13:26:16 +1000

    getters, setters, defaults and new methods for Calculator

--- src/automail/Calculator.java
+++ src/automail/Calculator.java
@@ -4,76 +4,83 @@ import com.unimelb.swen30006.wifimodem.WifiModem;
 
 import simulation.Building;
 
-import exceptions.ReinitiateCalculatorException;
-import exceptions.UninitiatedCalculatorException;
-
 public class Calculator {
 	private static final double movementCost = 5;
 	private static final double lookupCost = 0.1;
-	
+
 	private static double[] serviceFees = null;
-	
-	private static boolean isInitiated = false;
-	
-	private static double markupPercentage;
-	private static double activityUnitCost;
-	private static double chargeThreshold;
-	
-	
-	public static double calculateServiceFee(int floor) throws Exception {
+
+	private static double markupPercentage = 0;
+	private static double activityUnitPrice = 0;
+	private static double chargeThreshold = 0;
+
+
+	public static double calculateCharge(MailItem item) throws Exception {
+
+		double cost = calculateActivityCost(item) + calculateServiceFee(item);
+
+		return cost * (1 + markupPercentage);
+	}
+
+	public static double calculateServiceFee(MailItem item) throws Exception {
 		if(serviceFees == null) {
 			serviceFees = new double[Building.FLOORS];
 			for(int i=0; i<Building.FLOORS; i++) {
 				serviceFees[i] = 0;
 			}
 		}
-		
+
 		double lookupResult;
 		try {
-			lookupResult = WifiModem.getInstance(floor).forwardCallToAPI_LookupPrice(floor);
+			lookupResult = WifiModem.getInstance(item.getDestFloor()).forwardCallToAPI_LookupPrice(item.getDestFloor());
 		}
 		catch(Exception e) {
 			throw e;
 		}
-		
+
+		// record-keeping
+		item.recordLookup();
+		Accountant.recordLookup(lookupResult >= 0); // negative result means failure
+
+		// actually giving an answer
 		if(lookupResult < 0) {
-			Accountant.recordLookup(false);
-			return serviceFees[floor];
+			// failure: use the most recent record
+			return serviceFees[item.getDestFloor()];
 		}
 		else {
-			for(int i=0; i<Building.FLOORS; i++) {
-				if(fee == 0) {
-					fee = lookupResult;
-				}
-			}
-			Accountant.recordLookup(true);
+			// success: update the records in case of future failure
+			serviceFees[item.getDestFloor()] = lookupResult;
 			return lookupResult;
 		}
 	}
-	
-	public static double charge(double serviceFee,
-								int movementCount,
-								int lookupCount)
-					throws UninitiatedCalculatorException {
-		if(!isInitiated) {
-			throw new UninitiatedCalculatorException();
-		}
-		
-		double activityUnits = (movementCount * movementCost) +
-								(lookupCount * lookupCost);
-		
-		double activityCost = activityUnits * activityUnitCost;
-		
-		double cost = serviceFee + activityCost;
-		
-		return cost + (1 + markupPercentage);
+
+	public static double calculateActivityCost(MailItem item) {
+		return calculateActivityUnits(item) * activityUnitPrice;
 	}
-	
-	public static double getChargeThreshold() throws UninitiatedCalculatorException {
-		if(!isInitiated) {
-			throw new UninitiatedCalculatorException();
-		}
+	public static double calculateActivityUnits(MailItem item) {
+		return (movementCost * item.getMovementCount()) +
+				(lookupCost * item.getLookupCount());
+	}
+
+
+	public static double getChargeThreshold() {
 		return chargeThreshold;
 	}
-}
+	public static void setChargeThreshold(double chargeThreshold) {
+		Calculator.chargeThreshold = chargeThreshold;
+	}
 
+	public static double getActivityUnitPrice() {
+		return activityUnitPrice;
+	}
+	public static void setActivityUnitPrice(double activityUnitPrice) {
+		Calculator.activityUnitPrice = activityUnitPrice;
+	}
+
+	public static double getMarkupPercentage() {
+		return markupPercentage;
+	}
+	public static void setMarkupPercentage(double markupPercentage) {
+		Calculator.markupPercentage = markupPercentage;
+	}
+}
